c67be8dc3d59db1c5d2ead5d10741618c5456002
jithin0000 <mbjithinbabu@gmail.com>
2020-02-02 21:36:43 +0530

    added crud view method

--- post/urls.py
+++ post/urls.py
@@ -4,7 +4,11 @@ from post import views
 
 
 urlpatterns = [
-        
-        path('', views.index)
 
-        ]
+    path('', views.home, name="home"),
+    path('<int:pk>', views.details, name="detail"),
+    path('add', views.addPost, name="add"),
+    path('update/<int:pk>', views.updatePost, name="update"),
+    path('delete/<int:pk>', views.deletePost, name="delete"),
+
+]
--- post/views.py
+++ post/views.py
@@ -2,7 +2,43 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def home(request):
     """ post index view """
 
-    return render(request, "post/home.html", {})
+    return render(request, "post/home.html", { 'title' : 'Django tutorial' })
+
+
+def details(request, pk):
+    """ detail view of post """
+
+    return render(request, "post/detail.html", { 'id' : pk })
+
+
+def addPost(request):
+    """ view for creating post """
+    return render(request, 'post/add_post.html', {  })
+
+def updatePost(request, pk):
+    """ view for updating post """
+    return render(request, 'post/update_post.html', { "id" : pk })
+
+def deletePost(request, pk):
+    """ view for delete post """
+    return render(request, 'post/delete_post.html', { "id" : pk })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
